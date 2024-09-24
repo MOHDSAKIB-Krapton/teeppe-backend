16a49154d224ea3169ce396c6b1223f2f95c1c72
Mohd Sakib <mohdsakib@krapton.com>
2024-09-24 19:52:27 +0530

    fixed cores

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { createServer, IncomingMessage, ServerResponse } from 'http';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
 
-let cachedServer: any;
-
-// Function to bootstrap the app locally (for development)
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS with default settings
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://your-frontend-domain.com'], // List of allowed origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Allowed HTTP methods
+    credentials: true, // Enable credentials if needed (e.g., cookies, authorization headers)
+  });
+
+  // Set global API prefix
   app.setGlobalPrefix('api/v1');
+
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
 }
